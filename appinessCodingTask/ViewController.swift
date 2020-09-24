199c1f07c7debd0f2fc52d87d6c0c7c8cbb48e5c
Ranganath <pranganath@msewa.com>
2020-09-24 22:06:14 +0530

    Initial Commit

---
+++ appinessCodingTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  appinessCodingTask
+//
+//  Created by Parimala Ranganath Velayudam on 24/09/20.
+//  Copyright © 2020 VPR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
